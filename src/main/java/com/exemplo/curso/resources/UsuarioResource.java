bfd163e0fe6e0b189c618819f28e012f79cc1f4d
Matheus Glauber Rodrigues Jordão <glauber.jordao1995@outlook.com>
2020-10-24 00:13:28 -0300

    Alterado o findAll para o comportamento normal, e criado o findById

--- src/main/java/com/exemplo/curso/resources/UsuarioResource.java
+++ src/main/java/com/exemplo/curso/resources/UsuarioResource.java
@@ -1,19 +1,32 @@
 package com.exemplo.curso.resources;
 
 import com.exemplo.curso.entities.Usuario;
+import com.exemplo.curso.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/usuarios")
 public class UsuarioResource {
 
-    @GetMapping
-    public ResponseEntity<Usuario> findAll(){
-        Usuario usr = new Usuario(1L, "Maria", "maria@gmail.com", "81999999999", "123456");
+    @Autowired
+    private UsuarioService usuarioService;
 
+    @GetMapping
+    public ResponseEntity<List<Usuario>> findAll(){
+        List<Usuario> listaDeUsuarios = usuarioService.findAll();
+        return ResponseEntity.ok().body(listaDeUsuarios);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Usuario> findById(@PathVariable long id){
+        Usuario usr = usuarioService.findById(id);
         return ResponseEntity.ok().body(usr);
     }
 
