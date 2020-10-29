06194de02c30c98ef792111ab9ee2e6e0cf63f21
Matheus Glauber Rodrigues Jordão <glauber.jordao1995@outlook.com>
2020-10-28 23:04:45 -0300

    Entidade produto criado

---
+++ src/main/java/com/exemplo/curso/entities/Produto.java
@@ -0,0 +1,94 @@
+package com.exemplo.curso.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "produtos")
+public class Produto implements Serializable {
+
+   private static final long serialVersionUID = -8786242134181450984L;
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+
+   private String nome;
+   private String descricao;
+   private Double preco;
+   private String imgUrl;
+
+   @Transient
+   private Set<Categoria> categorias = new HashSet<>();
+
+    public Produto() {
+    }
+
+    public Produto(Long id, String nome, String descricao, Double preco, String imgUrl) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.imgUrl = imgUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public Set<Categoria> getCategorias() {
+        return categorias;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return Objects.equals(id, produto.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
