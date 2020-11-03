83864009769034484ad4786680b83e3757757e3a
Matheus Glauber Rodrigues Jordão <glauber.jordao1995@outlook.com>
2020-11-03 20:01:51 -0300

    Criação do PedidoProduto, Associação ManyToMany (muitos para muitos)

---
+++ src/main/java/com/exemplo/curso/entities/PedidoProduto.java
@@ -0,0 +1,76 @@
+package com.exemplo.curso.entities;
+
+import com.exemplo.curso.entities.pk.PedidoProdutoPK;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "pedido_produto")
+public class PedidoProduto implements Serializable {
+
+    private static final long serialVersionUID = 8622296738695664265L;
+
+    @EmbeddedId
+    private PedidoProdutoPK id;
+    private Integer quantidade;
+    private Double preco;
+
+    public PedidoProduto() {
+    }
+
+    public PedidoProduto(Pedido pedido, Produto produto, Integer quantidade, Double preco) {
+        id.setPedido(pedido);
+        id.setProduto(produto);
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+
+    public Pedido getPedido(){
+        return id.getPedido();
+    }
+
+    public void setPedido(Pedido pedido){
+        id.setPedido(pedido);
+    }
+
+    public Produto getProduto(){
+        return id.getProduto();
+    }
+
+    public void setProduto(Produto produto){
+        id.setProduto(produto);
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PedidoProduto that = (PedidoProduto) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
